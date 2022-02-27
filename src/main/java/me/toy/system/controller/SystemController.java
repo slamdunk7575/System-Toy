d37200bb1268751f77582e00cbb2ee0ac3e2d8d6
slamdunk7575 <slamdunk7575@gmail.com>
2022-02-27 18:30:07 +0900

    (feat) health 체크 추가

--- src/main/java/me/toy/system/controller/SystemController.java
+++ src/main/java/me/toy/system/controller/SystemController.java
@@ -1,10 +1,17 @@
 package me.toy.system.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SystemController {
+
+    @GetMapping("/")
+    public ResponseEntity<Void> health() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @GetMapping("/hello")
     public String hello() {
